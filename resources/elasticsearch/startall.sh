3c2d0b125945b41cf3d79930a65d03f1ba0de04e
Hong, Dewey <doohee323@gmail.com>
2016-03-06 23:48:25 -0800

    es group-by query

--- resources/elasticsearch/startall.sh
+++ resources/elasticsearch/startall.sh
@@ -1,11 +1,13 @@
 cd $PROJ_DIR 
-cd ./node1 && ./start.sh &
+echo cd $PROJ_DIR
+
+cd node1 && ./start.sh &
 sleep 1
 cd $PROJ_DIR
-cd ./node2 && ./start.sh &
+cd node2 && ./start.sh &
 sleep 1
 cd $PROJ_DIR
-cd ./node3 && ./start.sh &
+cd node3 && ./start.sh &
 
 #cd $PROJ_DIR/logstash-1.4.0; bin/logstash -f logstash-mixpanel.conf &
 
--- resources/elasticsearch/stopall.sh
+++ resources/elasticsearch/stopall.sh
@@ -1,9 +1,11 @@
 cd $PROJ_DIR
-cd ./node1 && ./stop.sh &
+echo cd $PROJ_DIR
+
+cd node1 && ./stop.sh &
 cd $PROJ_DIR
-cd ./node2 && ./stop.sh &
+cd node2 && ./stop.sh &
 cd $PROJ_DIR
-cd ./node3 && ./stop.sh &
+cd node3 && ./stop.sh &
 
 #cd $PROJ_DIR/logstash-1.4.0; bin/logstash -f logstash-mixpanel.conf &
 
